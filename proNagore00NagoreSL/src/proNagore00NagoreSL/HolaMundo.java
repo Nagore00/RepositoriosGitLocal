aa9e4bab3eb4cd02abf6f72aeba05b326a0cad82
Nagore <Nagore@Nagore>
2023-04-24 12:22:01 +0200

    segunda prueba

--- proNagore00NagoreSL/src/proNagore00NagoreSL/HolaMundo.java
+++ proNagore00NagoreSL/src/proNagore00NagoreSL/HolaMundo.java
@@ -3,7 +3,8 @@ package proNagore00NagoreSL;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo, Soy Nagore!");
+		System.out.println("Hola Mundo!");
+		System.out.println("Adios Mundo!");
 
 	}
 
