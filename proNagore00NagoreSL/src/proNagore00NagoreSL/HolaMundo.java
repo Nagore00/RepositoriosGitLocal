da69e37641647c06fa768270d44e9117a9e927cf
Nagore <Nagore@Nagore>
2023-04-25 09:18:50 +0200

    Conflicto

--- proNagore00NagoreSL/src/proNagore00NagoreSL/HolaMundo.java
+++ proNagore00NagoreSL/src/proNagore00NagoreSL/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
 		System.out.println("Tercer cambio");
-
+		System.out.println("Conflicto segundo usuario");
 	}
 
 }
